16163d4afa952c953335efc07610238a3c3e08d9
advaithsuryaP <padvaithsurya@gmail.com>
2024-04-11 18:26:27 -0400

    add sign-in flow

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -27,6 +28,45 @@ router.post('/register', (req, res, next) => {
 				});
 			});
 	});
+});
+
+router.post('/login', (req, res, next) => {
+	let fetchedUser;
+	User.findOne({ email: req.body.email })
+		.then((user) => {
+			console.log(user);
+			if (!user) {
+				return res.status(401).json({
+					message: 'Invalid email',
+				});
+			}
+			fetchedUser = user;
+			return bcrypt.compare(req.body.password, user.password);
+		})
+		.then((result) => {
+			if (!result) {
+				return res.status(401).json({
+					message: 'Invalid password',
+				});
+			}
+			const token = jwt.sign(
+				{ email: fetchedUser.email, userId: fetchedUser._id },
+				'secret_for_auth_seng645',
+				{
+					expiresIn: '1h',
+				}
+			);
+			return res.status(200).json({
+				message: 'SignIn successfull.',
+				data: '',
+				token: token,
+			});
+		})
+		.catch((err) => {
+			return res.status(401).json({
+				message: 'Some unknown error occured. Try again later.',
+			});
+		});
 });
 
 module.exports = router;
